411b6691e4c6505f53f0a017b97669450c1fb86f
bschenerman <ben.schenerman@gmail.com>
2019-03-05 13:49:47 -0500

    ♿ Add fallback support for share btn

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,11 +4,26 @@
     const shareBtnFeedback = shareBtnContainer.children[3];
     
     const copyPostLink = ()=>{
-        const link = shareBtnContainer.children[1];
-        link.select()
-        document.execCommand('copy');
-        let feedbackMsg = 'link copied to clipboard'
-        shareBtnFeedback.innerHTML = feedbackMsg;
+        const link = shareBtnContainer.children[1]; //input element containing post link
+        
+        const feedbackMsg = (str = '')=>{ //set feedbackMsg
+            shareBtnFeedback.textContent = str;
+            return str;
+        }        
+
+        if( !document.queryCommandSupported('copy') ){ //fallback support
+            link.classList.remove('visually-hidden');
+            link.select();
+            feedbackMsg('Copy link to share');
+        } else { //copy to clipboard
+            link.select();
+            document.execCommand('copy');
+            feedbackMsg('link copied to clipboard');
+        }
+        
+        window.setTimeout(()=>{ //clear feedbackMsg after 5s
+            feedbackMsg();
+        }, 5000)
     }
     
     shareBtn.addEventListener('click', copyPostLink);
