2dcb1930e557128f87ba115ecf53a2be2400bb47
bschenerman <ben.schenerman@gmail.com>
2019-03-05 14:39:30 -0500

    💄 Add fonts and typesetting

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,36 @@
 {
-    const shareBtnContainer = document.querySelector('div.share-post');
-    const shareBtn = shareBtnContainer.children[2];
-    const shareBtnFeedback = shareBtnContainer.children[3];
-    
-    const copyPostLink = ()=>{
-        const link = shareBtnContainer.children[1]; //input element containing post link
-        
-        const feedbackMsg = (str = '')=>{ //set feedbackMsg
-            shareBtnFeedback.textContent = str;
-            return str;
-        }        
 
-        if( !document.queryCommandSupported('copy') ){ //fallback support
-            link.classList.remove('visually-hidden');
-            link.select();
-            feedbackMsg('Copy link to share');
-        } else { //copy to clipboard
-            link.select();
-            document.execCommand('copy');
-            feedbackMsg('link copied to clipboard');
+    const formatShareBtn = ()=>{
+        const shareBtnContainer = document.querySelector('div.share-post');
+        const shareBtn = shareBtnContainer.children[2];
+        const shareBtnFeedback = shareBtnContainer.children[3];
+        
+        const copyPostLink = ()=>{
+            const link = shareBtnContainer.children[1]; //input element containing post link
+            
+            const feedbackMsg = (str = '')=>{ //set feedbackMsg
+                shareBtnFeedback.textContent = str;
+                return str;
+            }        
+
+            if( !document.queryCommandSupported('copy') ){ //fallback support
+                link.classList.remove('visually-hidden');
+                link.select();
+                feedbackMsg('Copy link to share');
+            } else { //copy to clipboard
+                link.select();
+                document.execCommand('copy');
+                feedbackMsg('link copied to clipboard');
+            }
+            
+            window.setTimeout(()=>{ //clear feedbackMsg after 5s
+                feedbackMsg();
+            }, 5000)
         }
         
-        window.setTimeout(()=>{ //clear feedbackMsg after 5s
-            feedbackMsg();
-        }, 5000)
+        shareBtn.addEventListener('click', copyPostLink);
     }
-    
-    shareBtn.addEventListener('click', copyPostLink);
+
+    document.querySelector('div.share-post') && formatShareBtn();
     
 }
