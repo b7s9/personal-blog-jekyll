8fe79eda4a903b3286da1d06413f16ac9b9efae8
bschenerman <ben.schenerman@gmail.com>
2019-03-10 23:25:47 -0400

    ✨ Add snackbar for feedback msgs

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,49 @@
 {
-
+    const snackbar = document.getElementById('snackbar');
+    const snackbarLabel = snackbar.children[0];
+    ''.length == 0 && console.log()
+    /**
+     * 
+     * @param {html element} container where to put the feedback msg
+     * @param {String} str the feedback message
+     * @param {bool} priority 0=polite, 1=alert
+     * @return String
+     */
+    const setFeedbackMsg = (container, str = '', priority = 0)=>{ //set feedbackMsg
+        if(str.length > 0){
+            snackbar.classList.remove('visually-hidden');
+        }else{
+            snackbar.classList.add('visually-hidden');            
+        }
+        container.textContent = str;
+        return str;
+    }
+    
+    // const setAlertMsg = (container, str = '', priority = '0')=>{ //set feedbackMsg
+    //     container.textContent = str;
+    //     return str;
+    // }
+    
     const formatShareBtn = ()=>{
         const shareBtnContainer = document.querySelector('div.share-post');
         const shareBtn = shareBtnContainer.children[2];
-        const shareBtnFeedback = shareBtnContainer.children[3];
         
         const copyPostLink = ()=>{
             const link = shareBtnContainer.children[1]; //input element containing post link
-            
-            const feedbackMsg = (str = '')=>{ //set feedbackMsg
-                shareBtnFeedback.textContent = str;
-                return str;
-            }        
-
+                                
             if( !document.queryCommandSupported('copy') ){ //fallback support
                 link.classList.remove('visually-hidden');
                 link.select();
-                feedbackMsg('Copy link to share');
+                setFeedbackMsg(snackbarLabel, 'Copy link to share');
             } else { //copy to clipboard
                 link.select();
                 document.execCommand('copy');
-                feedbackMsg('link copied to clipboard');
+                setFeedbackMsg(snackbarLabel, 'link copied to clipboard');
             }
             
             window.setTimeout(()=>{ //clear feedbackMsg after 5s
-                feedbackMsg();
-            }, 5000)
+                setFeedbackMsg(snackbarLabel);
+            }, 4000)
         }
         
         shareBtn.addEventListener('click', copyPostLink);
